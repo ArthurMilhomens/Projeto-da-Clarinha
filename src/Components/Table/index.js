85875757f876c97b1fb79323fb16363046cae3f0
Clara <maria.almeida@grupoicts.com.br>
2020-01-14 23:36:03 -0300

    criando map sem tabelinha

---
+++ src/Components/Table/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Table() {
+    const users = [
+        { name: "Clarinha", idade: 60, peso:"55kg", sexo: "Feminazi"},
+        { name: "Clarinha", idade: 60, peso:"55kg", sexo: "Feminazi"},
+        { name: "Clarinha", idade: 60, peso:"55kg", sexo: "Feminazi"},
+        { name: "Clarinha", idade: 60, peso:"55kg", sexo: "Feminazi"},
+    ]   
+    
+    const usersInfo = users.map(item => {
+        container[item.name] = item.idade, item.peso, item.sexo;
+        console.log(item.idade);
+    })
+    
+    
+    
+
+//   return (
+//       <Container>
+
+//       </Container>
+//   );
+}
